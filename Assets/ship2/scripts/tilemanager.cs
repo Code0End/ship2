385218cf5d0aa1a504ea4e3b364ff71b4f8c52b0
Code0End <lbenitesable@gmail.com>
2023-04-01 22:23:23 -0600

    agua infinita

---
+++ Assets/ship2/scripts/tilemanager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tilemanager : MonoBehaviour
+{
+    public Transform player;
+
+    public GameObject tile_prefab;
+
+    public Vector3 spawnPos;
+    public float spawnDistance = 10.0f;
+    public float tileLength = 40f;
+    private Vector3 moveDirection;
+    public int numTilesonScreen = 2;
+
+    private List<GameObject> activeTiles;
+
+    private void Start()
+    {
+        moveDirection = Vector3.forward;
+        activeTiles = new List<GameObject>();
+        spawnPos = new Vector3(0, 0, 0);
+        SpawnTile();      
+    }
+
+    void Update()
+    {
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        if (Vector3.Distance(player.position, spawnPos) > spawnDistance)
+        {
+            SpawnTile();
+            if (activeTiles.Count > numTilesonScreen)
+            {
+                DeleteTile();
+            }
+        }
+    }
+
+    private void SpawnTile()
+    {
+        GameObject tile = Instantiate(tile_prefab, new Vector3(player.position.x,0,player.position.z), Quaternion.identity);
+        activeTiles.Add(tile);
+        spawnPos += player.position;        
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+}
