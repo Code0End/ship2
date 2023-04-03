ccd8ef98d537ac37dc27c1186c0e979778d4ac04
gerorubio <vulcandro@hotmail.com>
2023-04-02 19:37:18 -0600

    Knockback

--- Assets/ship2/scripts/Enemy.cs
+++ Assets/ship2/scripts/Enemy.cs
@@ -2,28 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour {
-    private Rigidbody2D rb;
+    private Rigidbody rb;
     private Transform player;
     private SpriteRenderer sprite;
+    private Vector3 lastPosition;
+    private bool isMoving = true;
 
-    public float moveSpeed = 0.5f;
+    public float moveSpeed;
+    public float followDelay;
+    public float strenght = 0.5f;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = this.GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody>();
         sprite = this.GetComponent<SpriteRenderer>();
     }
 
     void Update() {
+
+        followDelay -= Time.deltaTime;
+
+        if (followDelay <= 0) {
+            lastPosition = player.position;
+            followDelay = 1f;
+        }
         // Sprite orientation
-        if(player.position.x > transform.position.x) {
+        if (player.position.x > transform.position.x) {
             sprite.flipX = true;
         } else {
             sprite.flipX = false;
         }
 
-        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q)) {
+            Knockback();
+        }
     }
+
+    private void FixedUpdate() {
+        // Movement
+        if (isMoving) {
+            transform.position = Vector3.MoveTowards(this.transform.position, lastPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Knockback() {
+        isMoving = false;
+        
+        Vector3 direction = (transform.position - player.transform.position).normalized;
+        rb.AddForce(direction * strenght, ForceMode.Impulse);
+        StartCoroutine(Reset());
+    }
+
+    public IEnumerator Reset() {
+        yield return new WaitForSeconds(0.1f);
+        rb.velocity = Vector3.zero;
+        isMoving = true;
+    }
+
 }
